5a49f2699de1e91610128e630a00fe9d23eac5c1
Jørgen Thorsnes <jorgen.thorsnes@gmail.com>
2022-02-16 23:11:39 +0100

    am bad at sorting

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
-console.log("hello")
+const yep = [
+	{
+        "check": "should be in the middle",
+		"created_at": "2022-02-16T11:16:04Z",
+		"published_at": "2022-01-16T11:17:07Z"
+	},
+	{
+        "check": "should be first",
+		"created_at": "2022-02-16T08:55:24Z",
+		"published_at": "2022-01-17T08:55:44Z"
+	},
+	{
+        "check": "should be last",
+		"created_at": "2022-02-14T11:43:00Z",
+		"published_at": "2021-02-14T11:45:24Z"
+	}
+]
 
-const tet = "tett"
-const jfkd = "n"
-
-const fhdhfkjds = "hfd"
-let h = 213
-
-let y = 233
-y++
-
-let uuu = "yr.no"
+yep.sort((a, b) => new Date(b.published_at) - new Date(a.published_at))
+console.log(yep)
