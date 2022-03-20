5c2c71c01b61a245197ffdfb5b498645a1de70f5
jorgtho <jorgen.thorsnes@gmail.com>
2022-03-20 22:00:52 +0100

    fdfd

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const yep = [
 	}
 ]
 
-console.log('This is a change too - indeeed')
+console.log('This is a change too yes - indeeed')
 
 /*
 yep.sort((a, b) => new Date(b.published_at) - new Date(a.published_at))
